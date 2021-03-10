5b46ea88f8386ea27995d543a37673f20b6fef3d
Isaac Zinda <me@isaaczinda.com>
2021-03-09 22:49:21 -0800

    add suffocated logic

--- board.py
+++ board.py
@@ -1,12 +1,6 @@
-# make object that represents board state **
-#  - place() --> board state
-# SERIALIZABLE
-
-
-# hard parts:
-#  - list of places you can't go
-#  - whether the game is over
-
+# TODO:
+# - whether the game is over
+# - when does AI pass (no more legal moves)
 
 # def StartOfGameBoard()
 #     [[EMPTY for w in range(width)] for h in range(height)]
@@ -17,7 +11,6 @@ import copy
 BLACK = 0
 WHITE = 1
 EMPTY = 2
-
 
 class BoardState:
     def __init__(self, board, turn, prevBoardState=None):
@@ -56,31 +49,134 @@ class BoardState:
 
         return self.turn == other.turn and self.board == other.board
 
-    def __leftOccupied(self, color, widthPos, heightPos):
-        if widthPos == 0:
+    # # def __removeTakenPieces(self):
+    #     ''' returns (new cleaned up board state, number pieces removed) '''
+
+    def _blockSuffocated(self, otherColor, widthPos, heightPos):
+        checked = []
+
+        toCheck = []
+
+        # enqueue 1
+
+        # check if its suffocated, if its not we're good!
+        if not all([
+            self._suffocatedLeft(otherColor, widthPos, heightPos),
+            self._suffocatedRight(otherColor, widthPos, heightPos),
+            self._suffocatedBelow(otherColor, widthPos, heightPos),
+            self._suffocatedAbove(otherColor, widthPos, heightPos),
+        ]):
+            return False
+
+        # If it is suffocated, check its neighbors
+        neighbors = self._getNeighbors(widthPos, heightPos, checked)
+
+
+
+        # If there aren't neighbors, its a singleton OR they've all been
+        # checked
+        if len(neighbors) == 0:
             return True
 
-        return self.board[widthPos][heightPos] == color
+        for neighbor in neighbors:
+            # If
+            if not self._blockSuffocated(otherColor, neighbor[0], neighbor[1]):
+                return False
 
 
-    def __rightOccupied(self, color, widthPos, heightPos):
-        if widthPos == self.width-1:
+
+        [ for neighbor in neighbors]
+
+
+    def _getNeighbors(self, widthPos, heightPos, exclude=[]):
+        color = self.board[widthPos][heightPos]
+
+        neighbors = []
+
+        # left
+        if widthPos != 0:
+            neighbors.append((widthPos-1, heightPos))
+
+        # right
+        if widthPos != self.width - 1:
+            neighbors.append((widthPos+1, heightPos))
+
+        # up
+        if heightPos != 0:
+            neighbors.append((widthPos, heightPos-1))
+
+        # down
+        if heightPos != self.height-1:
+            neighbors.append((widthPos, heightPos+1))
+
+        # remove excluded ones
+        return [t for t in neighbors if t not in exclude]
+
+
+    def _suffocatedLeft(self, otherColor, widthPos, heightPos):
+        # Returns True if we hit the left wall of the board
+        if widthPos == -1:
             return True
 
-        return self.board[widthPos][heightPos] == color
+        # If we hit our own color, keep going
+        if self.board[widthPos][heightPos] == (not otherColor):
+            return self._suffocatedLeft(otherColor, widthPos-1, heightPos)
 
-    def __aboveOccupied(self, color, widthPos, heightPos):
-        if heightPos == 0:
+        # If we hit the other color, it is suffocated
+        if self.board[widthPos][heightPos] == otherColor:
             return True
 
-        return self.board[widthPos][heightPos] == color
+        # Otherwise it's empty so we return False
+        return False
 
-    def __belowOccupied(self, color, widthPos, heightPos):
-        if heightPos == self.height-1:
+
+    def _suffocatedRight(self, otherColor, widthPos, heightPos):
+        # Returns True if we hit the left wall of the board
+        if widthPos == self.width:
             return True
 
-        return self.board[widthPos][heightPos] == color
+        # If we hit our own color, keep going
+        if self.board[widthPos][heightPos] == (not otherColor):
+            return self._suffocatedRight(otherColor, widthPos+1, heightPos)
 
+        # If we hit the other color, it is suffocated
+        if self.board[widthPos][heightPos] == otherColor:
+            return True
+
+        # Otherwise it's empty so we return False
+        return False
+
+    def _suffocatedAbove(self, otherColor, widthPos, heightPos):
+        # Returns True if we hit the left wall of the board
+        if heightPos == -1:
+            return True
+
+        # If we hit our own color, keep going
+        if self.board[widthPos][heightPos] == (not otherColor):
+            return self._suffocatedAbove(otherColor, widthPos, heightPos-1)
+
+        # If we hit the other color, it is suffocated
+        if self.board[widthPos][heightPos] == otherColor:
+            return True
+
+        # Otherwise it's empty so we return False
+        return False
+
+    def _suffocatedBelow(self, otherColor, widthPos, heightPos):
+        # Returns True if we hit the left wall of the board
+        if heightPos == self.height:
+            return True
+
+        # If we hit our own color, keep going
+        if self.board[widthPos][heightPos] == (not otherColor):
+            return self._suffocatedBelow(otherColor, widthPos, heightPos+1)
+
+        # If we hit the other color, it is suffocated
+        if self.board[widthPos][heightPos] == otherColor:
+            return True
+
+        # Otherwise it's empty so we return False
+        return False
 
     def legalMoves(self):
         """ Returns positions on the board which are empty and
@@ -96,7 +192,16 @@ class BoardState:
 
         for w in range(self.width):
             for h in range(self.height):
-                if self.__leftOccupied(otherColor, w, h) and self.__rightOccupied(otherColor, w, h) and self.__aboveOccupied(otherColor, w, h) and self.__belowOccupied(otherColor, w, h):
+                # Checks if this shit's surrounded, its not a legal move if
+                # surrounded. HOWEVER, it IS legal to play in a surrounded
+                # area if you take an opponent's piece.
+                if all([
+                    self.__stoneToLeft(otherColor, w, h),
+                    self.__stoneToRight(otherColor, w, h),
+                    self.__stoneAbove(otherColor, w, h),
+                    self.__stoneBelow(otherColor, w, h),
+                    self.__removeTakenPieces()[1] == 0
+                ]):
                     areEmpty[w][h] = False
 
         return areEmpty
@@ -114,6 +219,9 @@ class BoardState:
 
         newState = BoardState(newBoard, not self.turn, prevBoardState=self)
 
+        # Now "take" the pieces
+        newState, _ = newState.__removeTakenPieces()
+
         # now check to make sure this move doesn't repeat what the board
         # state was 2 turns ago. Since self is ~now~ 1 board state ago, we
         # just have to check self.prevBoardState.
@@ -122,6 +230,14 @@ class BoardState:
             return None
 
         return newState
+
+    def score(self, color):
+        # count up my own stones
+        numStones = sum([sum([elem == color for elem in row]) for row in self.board])
+
+        # count empty terratory that we occupy
+
+
 
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -1,25 +1,38 @@
 import board
 
-def test_ko():
-
-    b = board.BoardState([[board.EMPTY for i in range(4)] for j in range(4)], board.BLACK)
-
-    b = b.place(2, 0)
-
-    b = b.place(1, 0)
-
-    b = b.place(3, 1)
-
-    b = b.place(0, 1)
-
-    b = b.place(2, 2)
-
-    b = b.place(1, 2)
-
-    b = b.place(1, 1)
-
-    b = b.place(2, 1)
+def test_suffocated():
+    b = board.BoardState([
+        [board.BLACK, board.WHITE, board.EMPTY],
+        [board.WHITE, board.BLACK, board.WHITE],
+        [board.EMPTY, board.WHITE, board.EMPTY]
+    ], board.BLACK)
+    assert b._suffocatedLeft(board.WHITE, 1, 1)
+    assert b._suffocatedRight(board.WHITE, 1, 1)
+    assert b._suffocatedAbove(board.WHITE, 1, 1)
+    assert b._suffocatedBelow(board.WHITE, 1, 1)
+    assert b._suffocatedLeft(board.WHITE, 0, 0)
+    assert b._suffocatedRight(board.WHITE, 0, 0)
+    assert b._suffocatedAbove(board.WHITE, 0, 0)
+    assert b._suffocatedBelow(board.WHITE, 0, 0)
+    assert not b._suffocatedRight(board.BLACK, 1, 0)
 
 
-    # Illegal moves must return None
-    assert b.place(1, 1) == None
+    # b = b.place(2, 0)
+    # print(b)
+    # b = b.place(1, 0)
+    # print(b)
+    # b = b.place(3, 1)
+    # print(b)
+    # b = b.place(0, 1)
+    # print(b)
+    # b = b.place(2, 2)
+    # print(b)
+    # b = b.place(1, 2)
+    # print(b)
+    # b = b.place(1, 1)
+    # print(b)
+    # b = b.place(2, 1)
+    # print(b)
+
+    # # Illegal moves must return None
+    # assert b.place(1, 1) == None
